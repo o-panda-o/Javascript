4a2d3a57b5504378b98ab42b89dd4818f08a31fd
o_panda_o <emailofpanda@yahoo.com>
2018-11-26 21:06:28 +0530

    Introduction

---
+++ Introduction to Javascript for Developers/01. Introduction.js
@@ -0,0 +1,33 @@
+/**
+ * Javascript:
+ *  + is a light weight(small memory footprint, easy to interpret), 
+ *  + interpreted(no compilation required, instruction executed directly, no intermediate file formation), 
+ *  + object oriented language(model around objects),
+ *  + first class functions(function as values) containing & is best known as
+ *  + the scipting language(language that is designed for runtime)
+ *  for web pages, but it is used as many non-browser environments as well.
+ */
+
+
+ /**
+  * --- Uses of Javascript --- 
+  * 
+  * Client side development:
+  *     + Native Javascript
+  *     + jQuery
+  *     + Angular JS, React, Ember
+  * 
+  * Server side development:
+  *     + NodeJS
+  *     + Express
+  * Browser extensions
+  * 
+  * Desktop Application
+  * 
+  * Mobile Application
+  * 
+  * IOT Application
+  */
+ /**
+  * Also called ECMA Script
+  */
