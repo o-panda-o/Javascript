1b94b0667cff9dcf6220e802bcd5eecb658bfbb1
Bibhuti Bhusan Panda <bibhuti.bhusan@vedantu.com>
2019-04-16 21:19:29 +0530

    Boolean behaviour for different data types

---
+++ Javascript - Java Brains/Introduction to Javascript for Developers/16.BooleanFunctionBehaviour.js
@@ -0,0 +1,12 @@
+/**
+ * This code says how boolean acts around various data types
+ */
+console.log(Boolean(true)); // true
+console.log(Boolean(false));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(''));
+console.log(Boolean(NaN));
+console.log(Boolean(-5)); // true
+console.log(Boolean(0));
+console.log(Boolean(5)); // true
