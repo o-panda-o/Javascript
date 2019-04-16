a0ebfccf7fcfa0304b10b486be008bcf5bf47561
Bibhuti Bhusan Panda <bibhuti.bhusan@vedantu.com>
2019-04-16 20:43:16 +0530

    ES6 string changes

---
+++ Javascript - Java Brains/Introduction to Javascript for Developers/14.es6Concatnation.js
@@ -0,0 +1,10 @@
+/**
+ * - var is depreceated in ES6 so better to use let
+ * - also if we want to make an unchangable variable better to use const. 
+ */
+let firstName='Bibhuti';
+let lastName='Panda';
+
+const fullName=`${firstName} ${lastName}`;
+
+console.log(fullName);
