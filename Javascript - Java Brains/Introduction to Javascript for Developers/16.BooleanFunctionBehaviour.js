8605371d6eb39daea15eb44a4b7c005699684a79
Bibhuti Bhusan Panda <bibhuti.bhusan@vedantu.com>
2019-04-17 00:46:11 +0530

    Added changes for non-empty string

--- Javascript - Java Brains/Introduction to Javascript for Developers/16.BooleanFunctionBehaviour.js
+++ Javascript - Java Brains/Introduction to Javascript for Developers/16.BooleanFunctionBehaviour.js
@@ -6,6 +6,7 @@ console.log(Boolean(false));
 console.log(Boolean(null));
 console.log(Boolean(undefined));
 console.log(Boolean(''));
+console.log(Boolean(' ')); //true
 console.log(Boolean(NaN));
 console.log(Boolean(-5)); // true
 console.log(Boolean(0));
