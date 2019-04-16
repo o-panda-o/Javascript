52351395009f1371d99307855b58e871912edf35
Bibhuti Bhusan Panda <bibhuti.bhusan@vedantu.com>
2019-04-16 21:08:18 +0530

    Examples about weird behaviour of Javascript

---
+++ Javascript - Java Brains/Introduction to Javascript for Developers/15.weirdJSBehaviour.js
@@ -0,0 +1,9 @@
+let example1=parseInt("Hello 33 World 22");
+let example2=parseFloat("244 Hello 420");
+let example3=55.33333.toFixed(0);
+let example4=200.0.toFixed(2);
+
+console.log(example1,typeof example1);
+console.log(example2,typeof example2);
+console.log(example3,typeof example3);
+console.log(example4,typeof example4);
