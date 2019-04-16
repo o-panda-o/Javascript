f7b0c7e0c51dc1ca2ddf339493062c64284ad895
Bibhuti Bhusan Panda <bibhuti.bhusan@vedantu.com>
2019-04-17 01:01:40 +0530

    Array behaviour with ES6

--- Javascript - Java Brains/Introduction to Javascript for Developers/13.arrayMethods.js
+++ Javascript - Java Brains/Introduction to Javascript for Developers/13.arrayMethods.js
@@ -24,3 +24,18 @@ numbers.forEach(getElement);
 /**
  * array.forEach(function(item,index,array){})
  */
+
+ // Array behaviour
+ let example1=[1,2,3];
+ let example2=example1;
+ console.log('Printing contents of second example -- '+example2);
+ example2.push(4,5,6);
+ console.log('Changing of second example affects first one -- '+example1);
+ let example3=[...example1]; // ES6 syntax
+ example3.push(7,8,9);
+ console.log('Printing content of example 3 after taking the values from example 1 -- '+example3);
+ console.log('No changes in example 1 after changing example 3 -- '+example1);
+let example4=example1.map((element)=>{
+    return element;
+});
+console.log('After copying each and every content of example 1 to 4 -- '+example4);
