024c96d807a57762bcd5d2f2e02373a12ec13e6f
Bibhuti Bhusan Panda <bibhuti.bhusan@vedantu.com>
2019-04-19 00:40:55 +0530

    Testing

--- OOPs/02.factories.js
+++ OOPs/02.factories.js
@@ -1,7 +1,7 @@
 // Factory function
 function createCircle(radius){
     return {
-        radius, // In ES6 we don't need key value pair to do the same
+        radius, // In ES6 we don't need key value pair to do the same if the key and value have same name
         draw: function(){
             console.log('Drawing a circle');
         }
