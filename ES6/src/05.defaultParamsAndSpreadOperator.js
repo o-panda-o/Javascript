de675ae1c990e7b0a477c3063ac27208087c95db
Bibhuti Bhusan Panda <bibhuti.bhusan@vedantu.com>
2019-04-21 23:06:30 +0530

    Staging and default parameter

---
+++ ES6/src/05.defaultParamsAndSpreadOperator.js
@@ -0,0 +1,20 @@
+"use strict"
+
+// Default value assigning to parameters
+function greet(greeting='Hello'){
+    console.log(greeting);
+}
+
+greet();
+
+// spread operator
+let args=[1,2,3,4,5];
+let more_args=[6,7,8,9,0];
+
+function test(){
+    console.log(args+' '+more_args);
+}
+// ES5 way
+test.apply(null,args);
+// ES6
+test(...args,...more_args);
