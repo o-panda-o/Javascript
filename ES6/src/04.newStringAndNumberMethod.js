c4d0a6eb3f6717a90a4990aa4643e59ac0e95c7c
Bibhuti Bhusan Panda <bibhuti.bhusan@vedantu.com>
2019-04-21 22:59:17 +0530

    Added essential string and number methods

---
+++ ES6/src/04.newStringAndNumberMethod.js
@@ -0,0 +1,25 @@
+"use strict"
+
+let theString='Hello, my name is Bibhuti';
+
+console.log(theString.startsWith('Hello'));
+console.log(theString.endsWith('Bibhuti'));
+console.log(theString.includes('name'));
+
+// Hex
+console.log(0xFF);
+// Binary
+console.log(0b101011);
+// Octal
+console.log(0o543);
+
+// Number method
+
+console.log(Number.isFinite(Infinity));
+console.log(Number.isFinite(NaN));
+console.log(Number.isFinite(3));
+
+console.log(Number.isNaN(NaN));
+console.log(Number.isNaN(Infinity));
+
+console.log(Number.isInteger(2.3));
