2056d591f510a0d82e65da65545a11a34a8405ac
Bibhuti Bhusan Panda <bibhuti.bhusan@vedantu.com>
2019-04-26 00:59:14 +0530

    Pushing code regarding functional component

---
+++ ReactJS/01.functional-component/src/components/greet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Greet=() => <h1>Hello World</h1>
+
+
+/**
+ * In case of doing the below default import we can import it with
+ * any name inside the another component and create the tag for the 
+ * same. Like:
+ * 
+ * import <anyName> from './components/greet'
+ */
+// export default Greet
