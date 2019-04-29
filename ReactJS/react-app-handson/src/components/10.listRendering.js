55ed186f3b65fcc6e80e3ae340b0723dabb2e7f8
Bibhuti Bhusan Panda <bibhuti.bhusan@vedantu.com>
2019-04-30 00:32:34 +0530

    Adding code for index as a key

--- ReactJS/react-app-handson/src/components/10.listRendering.js
+++ ReactJS/react-app-handson/src/components/10.listRendering.js
@@ -2,8 +2,12 @@ import React from 'react'
 import Person from './10.person';
 
 function NameList() {
-    const names=['Bibhuti','Bhusan','Panda']
-    const nameList=names.map((name)=><h2>{name}</h2>);
+    const names=['Bibhuti','Bhusan','Panda','Bibhuti']
+    // Using `index as key` is a very bad idea, sometime it creates serious troubles.
+    // So it is better not to use them unless and until you don't have an option
+    const nameList=names.map((name,index)=><h2 key={index}>{index} -- {name}</h2>);
+
+
     const persons = [
         {
           id: 1,
@@ -24,7 +28,11 @@ function NameList() {
           skill: 'Vue'
         }
       ]
-    const personList=persons.map(person => <Person person={person} />);
+    // Adding key property value is important to avoid warnings. Also it is supposed
+    // to something which is unique to that list. Also `key` prop is not accessible as
+    // a prop in child component. It handles list iteration, addition and deletion 
+    // using a key efficiently, or else it has to use brute force approach for all.
+    const personList=persons.map(person => <Person key={person.id} person={person} />);
     return (
         <div>
             {/* 
@@ -44,8 +52,18 @@ function NameList() {
             {
                 personList
             }
+            {
+              nameList
+            }
         </div>
     )
 }
 
 export default NameList
+
+/**
+ * Use index as key for the following scenarios:(Avoid it as much as possible)
+ *  + The item of the list don't have a unique id
+ *  + The list is static, i.e. it won't change
+ *  + The list will never be reordered or filtered.
+ */
