db9630930c3866a1a1cbf3af75480cbf83b6f26a
Bibhuti Bhusan Panda <bibhuti.bhusan@vedantu.com>
2019-05-20 01:47:40 +0530

    Crash course by traversy media on redux

---
+++ ReactJS/redux-crash-course-traversy-media/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
