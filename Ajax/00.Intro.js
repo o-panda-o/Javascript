87e4a4b6fe0ebc96c431c6f261b94f9de408ea95
Bibhuti Bhusan Panda <bibhuti.bhusan@vedantu.com>
2019-09-23 16:20:06 +0530

    for other device

---
+++ Ajax/00.Intro.js
@@ -0,0 +1,27 @@
+/**
+ * Ajax:
+ *  + Asynchronous Javascript and XML
+ *  + Set of web technologies
+ *  + Send and receive data asynchronously
+ *  + Doesn't interfere with current web page
+ *  + JSON has replaced XML with most of the part
+ */
+
+/**
+ * XmlHttpRequest(XHR) Object:
+ *  + API in the form of an object
+ *  + Provided by browser's JS environment
+ *  + Methods transfer data between client/server
+ *  + Can be used with other protocol other than HTTP
+ *  + Can data other than XML(JSON, plain text etc.)
+ */
+
+/**
+ * Libraries & Other Methods:
+ *  + jQuery
+ *  + Axios
+ *  + Superagent
+ *  + Fetch API
+ *  + Prototype
+ *  + Node HTTP
+ */
